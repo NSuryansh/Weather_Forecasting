8fb14c9464c90087fc21dd1062793a757a011e04
NSuryansh <158190316+NSuryansh@users.noreply.github.com>
2024-02-09 22:02:17 +0530

    Update Weather Forcater.js

--- Weather Forcater.js
+++ Weather Forcater.js
@@ -1,14 +1,14 @@
-let c1 = 0
-let c2 = 0
-let c3 = 0
-let c4 = 0
+let c1 = 0//counter to run the weather app for the first time 
+let c2 = 0//counter for getting the latitude of Indore
+let c3 = 0//counter for getting the longitude of Indore
+let c4 = 0//counter for getting the time of Indore
 if (c1 == 0) {
     weather()
     c1++
 }
 
 
-
+//Function for getting the latitude of the city
 async function main() {
     let city
     if (c2 == 0) {
@@ -20,9 +20,9 @@ async function main() {
     let event = document.getElementById("search_city")
     let response = await fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=1f1d9b9875ca928d7b05d92de2787ec6");
     data = await response.json();
-    // console.log(data);
     return data[0].lat
 }
+//Function for getting the longitude of the city
 async function main2() {
     let city
     if (c3 == 0) {
@@ -33,10 +33,9 @@ async function main2() {
     }
     let response = await fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=1f1d9b9875ca928d7b05d92de2787ec6");
     data = await response.json();
-    // console.log(data);
     return data[0].lon
 }
-
+//Function to display the weather conditions of the city you have searched for 
 async function weather() {
     let temp1 = await main()
     let temp2 = await main2()
@@ -51,12 +50,9 @@ async function weather() {
     let add_city_name = document.getElementById("city_name")
     city = city.charAt(0).toUpperCase() + city.slice(1);
     add_city_name.innerText = city
-    // console.log(temp)
-    // console.log(temp2);
 
     let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + temp1 + "&lon=" + temp2 + "&appid=1f1d9b9875ca928d7b05d92de2787ec6")
     data = await response.json();
-    // console.log(data)
 
     let temperature = data.main.temp
     temperature -= 273.15
@@ -64,7 +60,6 @@ async function weather() {
     let temp = document.getElementById("temp")
     temp.innerText = " Temp: "
     temp.innerText = temp.innerText + temperature + " C"
-    // console.log(temperature);
 
     let feel_like = data.main.feels_like
     feel_like -= 273.15
@@ -72,37 +67,31 @@ async function weather() {
     let feel = document.getElementById("feel_like")
     feel.innerText = " Feel like: "
     feel.innerText = feel.innerText + feel_like + " C"
-    // console.log(feel_like);
 
     let humidity = data.main.humidity
     let output_humidity = document.getElementById("humidity")
     output_humidity.innerText = " Humidity: "
     output_humidity.innerText = output_humidity.innerText + humidity
-    // console.log(humidity);
 
     let visibility = data.visibility
     let output_visiblity = document.getElementById("visibility")
     output_visiblity.innerText = " Visibility: "
     output_visiblity.innerText = output_visiblity.innerText + visibility
-    // console.log(visibility);
 
     let wind_speed = data.wind.speed
     let output_speed = document.getElementById("speed")
     output_speed.innerText = " Wind speed: "
     output_speed.innerText = output_speed.innerText + wind_speed
-    // console.log(wind_speed);
 
     let wind_deg = data.wind.deg
     let output_dir = document.getElementById("deg")
     output_dir.innerText = " Wind Directionn: "
     output_dir.innerText = output_dir.innerText + wind_deg
-    // console.log(wind_deg);
 
     let description = data.weather[0].description
     let output_description = document.getElementById("description")
     description = description.charAt(0).toUpperCase() + description.slice(1);
     output_description.innerText = description
-    console.log(description);
 
     const time_response = await fetch("https://api.api-ninjas.com/v1/worldtime?city=" + city, {
         method: 'GET',
@@ -110,15 +99,12 @@ async function weather() {
             'X-Api-Key': 'NTJdiJOZXdXNMiXZkW6Xjg==Ilokw2VkztShBC21',
             'Content-Type': 'application/json'
         }
-    });
+    });//getting the time of the city 
 
     let time_data = await time_response.json();
-    console.log(time_data)
     let time = time_data.hour
     let main_weather = data.weather[0].main
-    console.log(time)
-    console.log(main_weather)
-
+    //to change the background of the box in which the weather of the city you searched is displayed  
     if (time === "20" || time === "21" || time ==="22 "|| time === "19" || time === "23" || time == "00" || time === "01" || time === "02" || time === "03" || time === "04" || time === "05" || time === "06") {
         if (main_weather === "Clouds" || main_weather === "Smoke") {
             document.getElementById("output1").style.backgroundImage = "url('nightclouds.png')";
@@ -155,7 +141,7 @@ async function weather() {
     
 
 }
-
+//adding the event listener for Enter
 document.getElementById("search_city").addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
         weather();
@@ -167,7 +153,7 @@ document.getElementById("search_city").addEventListener("keyup", function (event
 let c = 0;
 let c11 = 0;
 hello()
-
+//calling the function for diaplaying the weather of the famous cities five times
 async function hello() {
     for (let i = 0; i < 5; i++) {
         await famous_weatherPr()
@@ -175,7 +161,7 @@ async function hello() {
 }
 
 
-
+//Function for diaplaying the weather of the famous cities
 async function famous_weatherPr() {
     let city = ["Paris", "Mumbai", "Delhi", "London", "New york"]
     let t = ["tempPr", "tempMum", "tempDel", "tempLn", "tempNy"]
@@ -188,7 +174,6 @@ async function famous_weatherPr() {
     let ids = ["paris", "mumbai", "delhi", "london", "new_york"]
 
     let response1 = await fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city[c] + "&limit=5&appid=1f1d9b9875ca928d7b05d92de2787ec6");
-    // console.log(city[c])
     data1 = await response1.json();
     let temp1 = data1[0].lat
     let temp2 = data1[0].lon
@@ -203,7 +188,6 @@ async function famous_weatherPr() {
     let temp = document.getElementById(t[c])
     temp.innerText = " Temp: "
     temp.innerText = temp.innerText + temperature + " C"
-    // console.log(temperature);
 
     let feel_like = data.main.feels_like
     feel_like -= 273.15
@@ -211,38 +195,32 @@ async function famous_weatherPr() {
     let feel = document.getElementById(fl[c])
     feel.innerText = " Feel like: "
     feel.innerText = feel.innerText + feel_like + " C"
-    // console.log(feel_like);
 
     let humidity = data.main.humidity
     let output_humidity = document.getElementById(h[c])
     output_humidity.innerText = " Humidity: "
     output_humidity.innerText = output_humidity.innerText + humidity
-    // console.log(humidity);
 
     let visibility = data.visibility
     let output_visiblity = document.getElementById(v[c])
     output_visiblity.innerText = " Visibility: "
     output_visiblity.innerText = output_visiblity.innerText + visibility
-    // console.log(visibility);
 
     let wind_speed = data.wind.speed
     let output_speed = document.getElementById(s[c])
     output_speed.innerText = " Wind speed: "
     output_speed.innerText = output_speed.innerText + wind_speed
-    // console.log(wind_speed);
 
     let wind_deg = data.wind.deg
     let output_dir = document.getElementById(de[c])
     output_dir.innerText = " Wind Directionn: "
     output_dir.innerText = output_dir.innerText + wind_deg
-    // console.log(wind_deg);
 
     let description = data.weather[0].description
     description = description.charAt(0).toUpperCase() + description.slice(1);
     document.getElementById(descr[c]).innerText = description
 
     let main = data.weather[0].main
-    console.log(main);
 
     let time = background_time()
      
@@ -278,9 +256,10 @@ async function famous_weatherPr() {
 
 c++;
 }
+
+//Function to get the time of the famous cities 
 async function background_time() {
     let city = ["Paris", "Mumbai", "Delhi", "London", "New york"]
-    // console.log(city[c11])
     const time_response = await fetch("https://api.api-ninjas.com/v1/worldtime?city=" + city[c11], {
         method: 'GET',
         headers: {
@@ -290,24 +269,8 @@ async function background_time() {
     });
 
     let time_data = await time_response.json();
-    // console.log(time_data.hour);
     return time_data.hour
 
 }
-// async function background_time(city11) {
-//     // console.log(city[c11])
-//     const time_response = await fetch("https://api.api-ninjas.com/v1/worldtime?city=" + city11, {
-//         method: 'GET',
-//         headers: {
-//             'X-Api-Key': 'NTJdiJOZXdXNMiXZkW6Xjg==Ilokw2VkztShBC21',
-//             'Content-Type': 'application/json'
-//         }
-//     });
-
-//     let time_data = await time_response.json();
-//     // console.log(time_data.hour);
-//     return time_data.hour
-
-// }
 
 
